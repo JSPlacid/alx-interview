1bdc344c1d34eafd33e048f63de23309ddf54b25
JSPlacid <ojoolusegun17@gmail.com>
2023-07-07 06:04:02 -0700

    unlock interview question

---
+++ 0x01-lockboxes/0-lockboxes_2nd-app.py
@@ -0,0 +1,19 @@
+def canUnlockAll(boxes):
+    n = len(boxes)
+    unlocked = [False] * n
+    unlocked[0] = True  # The first box is unlocked by default
+    keys = set(boxes[0])  # Collecting keys from the first box
+
+    # Iterate through the unlocked boxes until no more new keys are found
+    while True:
+        new_keys = set()
+        for box_num in range(1, n):
+            if not unlocked[box_num] and box_num in keys:
+                unlocked[box_num] = True
+                new_keys.update(boxes[box_num])
+        keys.update(new_keys)
+        if not new_keys:
+            break
+
+    return all(unlocked)
+
