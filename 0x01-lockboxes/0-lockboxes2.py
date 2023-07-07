7627eb38169e9df7f32eb19d493a9141150127e9
JSPlacid <ojoolusegun17@gmail.com>
2023-07-07 06:15:16 -0700

    lockbox interview que

---
+++ 0x01-lockboxes/0-lockboxes2.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""lock boxes"""
+
+def canUnlockAll(boxes):
+    n = len(boxes)
+    unlocked = [False] * n
+    unlocked[0] = True  # The first box is unlocked by default
+    keys = boxes[0]  # Collecting keys from the first box
+
+    # Iterate through the unlocked boxes until no more new keys are found
+    for i in range(n):
+        if unlocked[i]:
+            for key in boxes[i]:
+                if key < n and not unlocked[key]:
+                    unlocked[key] = True
+                    keys.extend(boxes[key])
+
+    return all(unlocked)
+
