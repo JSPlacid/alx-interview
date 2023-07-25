d4c7e091e22b03bc4df557a510b6c3b8471b98e4
JSPlacid <ojoolusegun17@gmail.com>
2023-07-24 19:57:04 -0700

    log parse

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,47 +1,44 @@
 #!/usr/bin/python3
-```A script to parse http logs```
+"""Log Parser"""
 import sys
-from collections import defaultdict
 
 
-def print_metrics(total_size, status_codes):
-    print(f"Total file size: File size: {total_size}")
-    for code in sorted(status_codes.keys()):
-        print(f"{code}: {status_codes[code]}")
+if __name__ == '__main__':
+    file_size = [0]
+    status_codes = {200: 0, 301: 0, 400: 0, 401: 0,
+                    403: 0, 404: 0, 405: 0, 500: 0}
 
-def main():
-    total_size = 0
-    status_codes = defaultdict(int)
-    line_count = 0
+    def print_stats():
+        """ Print statistics """
+        print('File size: {}'.format(file_size[0]))
+        for key in sorted(status_codes.keys()):
+            if status_codes[key]:
+                print('{}: {}'.format(key, status_codes[key]))
 
+    def parse_line(line):
+        """ Checks the line for matches """
+        try:
+            line = line[:-1]
+            word = line.split(' ')
+            # File size is last parameter on stdout
+            file_size[0] += int(word[-1])
+            # Status code comes before file size
+            status_code = int(word[-2])
+            # Move through dictionary of status codes
+            if status_code in status_codes:
+                status_codes[status_code] += 1
+        except BaseException:
+            pass
+
+    linenum = 1
     try:
         for line in sys.stdin:
-            line = line.strip()
-            parts = line.split()
-            if len(parts) != 7:
-                continue
-
-            ip, _, _, method, path, _, status_code, file_size = parts
-
-            if method != 'GET' or not path.startswith('/projects/'):
-                continue
-
-            try:
-                file_size = int(file_size)
-            except ValueError:
-                continue
-
-            total_size += file_size
-            status_codes[status_code] += 1
-
-            line_count += 1
-
-            if line_count % 10 == 0:
-                print_metrics(total_size, status_codes)
-
+            parse_line(line)
+            """ print after every 10 lines """
+            if linenum % 10 == 0:
+                print_stats()
+            linenum += 1
     except KeyboardInterrupt:
-        print_metrics(total_size, status_codes)
-
-if __name__ == "__main__":
-    main()
-
+        print_stats()
+        raise
+    print_stats()
